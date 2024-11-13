bbe47e8112dc5308f0882c17731247f1ffb349ce
bihurin31204 <140156816+bihurin31204@users.noreply.github.com>
2024-11-13 21:45:23 +0700

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var userCollection *mongo.Collection
 // Middleware untuk mengatasi CORS
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "https://pawm-final-fe.vercel.app")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
